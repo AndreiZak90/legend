5c359e1060805d8f44c008685a122b83859378ff
AndreiZak <zak-90@inbox.ru>
2023-08-05 19:42:56 +0300

    commit five

--- src/test/Character.test.js
+++ src/test/Character.test.js
@@ -1,19 +1,9 @@
-import Daemon from "../js/Daemon";
-
-test("Class check fail", () => {
-  const name = "name";
-  const typee = "Bowman";
-  const examination = { name: "name", type: "Bowman", health: 100, level: 1 };
-
-  const character = new Daemon(name, typee);
-  expect(character).toEqual(examination);
-});
+import Zombie from "../js/Zombie";
 
 test("Checking the length of the introduced name", () => {
   const name = "nameLongBigLong";
   const typee = "Bowman";
-
-  expect(() => new Daemon(name, typee)).toThrowError(
+  expect(() => new Zombie(name, typee)).toThrowError(
     "Имя должно быть строкой от 2 до 10 символов"
   );
 });
@@ -22,13 +12,13 @@ test("Checking the introduced type", () => {
   const name = "name";
   const typee = "InvalidType";
 
-  expect(() => new Daemon(name, typee)).toThrowError("Ошибка");
+  expect(() => new Zombie(name, typee)).toThrowError("Ошибка");
 });
 
 test("Checking the standard of living", () => {
   const name = "name";
   const typee = "Bowman";
-  const newClass = new Daemon(name, typee);
+  const newClass = new Zombie(name, typee);
   newClass.health = 0;
   expect(() => newClass.levelUp()).toThrow("Нельзя повысить левел умершего");
 });
@@ -36,11 +26,37 @@ test("Checking the standard of living", () => {
 test("Checking the standard of living", () => {
   const name = "name";
   const typee = "Bowman";
-  const newClass = new Daemon(name, typee);
+  const newClass = new Zombie(name, typee);
   expect(() => newClass.damage(-10)).toThrow(
     "не является символом или значение меньше нуля"
   );
   expect(() => newClass.damage("number")).toThrow(
     "не является символом или значение меньше нуля"
   );
+});
+
+test("should increase level and reset health, attack, and defence", () => {
+  const zombiLEVEL = new Zombie("name", "Bowman");
+  zombiLEVEL.levelUp();
+
+  expect(zombiLEVEL.level).toBe(2);
+  expect(zombiLEVEL.health).toBe(100);
+  expect(zombiLEVEL.attack).toBeCloseTo(48, 0);
+  expect(zombiLEVEL.defence).toBeCloseTo(12, 0);
+});
+
+test("test levelUp умершего", () => {
+  expect(() => {
+    const newClass = new Zombie("name", "Bowman");
+    newClass.health = 0;
+    newClass.levelUp();
+  }).toThrow("Нельзя повысить левел умершего");
+});
+
+test("should not reduce health below 0 on damage()", () => {
+  const newClass = new Zombie("name", "Zombie");
+  newClass.health = 10;
+  const damage = 20;
+  newClass.damage(damage);
+  expect(newClass.health).toEqual(0);
 });
--- src/test/Zombie.test.js
+++ src/test/Zombie.test.js
@@ -1,4 +1,5 @@
 import Zombie from "../js/Zombie";
+
 test("Class check Zombie", () => {
   const name = "name";
   const typee = "Zombie";
@@ -10,29 +11,6 @@ test("Class check Zombie", () => {
     attack: 40,
     defence: 10,
   };
-
-  const zombie = new Zombie(name, typee);
-  expect(zombie).toEqual(examination);
-});
-
-test("should increase level and reset health, attack, and defence", () => {
-  const name = "name";
-  const typee = "Zombie";
-  const newClass = new Character(name, typee);
-  newClass.levelUp();
-
-  expect(newClass.level).toBe(2);
-  expect(newClass.health).toBe(100);
-  expect(newClass.attack).toBeCloseTo(12, 0);
-  expect(newClass.defence).toBeCloseTo(48, 0);
-});
-
-test("should not reduce health below 0 on damage()", () => {
-  const name = "name";
-  const typee = "Zombie";
-  const newClass = new Character(name, typee);
-  newClass.health = 10;
-  const damage = 20;
-  newClass.damage(damage);
-  expect(newClass.health).toBe(0);
+  const zombil = new Zombie(name, typee);
+  expect(zombil).toEqual(examination);
 });
